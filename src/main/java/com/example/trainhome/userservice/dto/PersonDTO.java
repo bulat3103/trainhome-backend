864112a9810b56c312b1e9b6a900f215dee16ed0
ivanz <zhamwan@gmail.com>
2022-11-22 16:02:30 +0300

    person service

--- src/main/java/com/example/trainhome/userservice/dto/PersonDTO.java
+++ src/main/java/com/example/trainhome/userservice/dto/PersonDTO.java
@@ -1,5 +1,6 @@
 package com.example.trainhome.userservice.dto;
 
+import com.example.trainhome.userservice.entities.Person;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,10 +9,16 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 public class PersonDTO {
+    private Long id;
     private String name;
     private String image;
     private String phoneNumber;
     private String email;
     private Date birthday;
     private boolean sex;
+
+    public static PersonDTO PersonToPersonDTO(Person person){
+        return new PersonDTO(person.getId(), person.getName(), person.getImage(),
+                person.getPhoneNumber(), person.getEmail(), person.getBirthday(), person.isSex());
+    }
 }
