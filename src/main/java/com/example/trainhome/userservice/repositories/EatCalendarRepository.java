18466f35e769810362b0df31924f19cccbb5ac88
admin <hafizov.bulat2002@gmail.com>
2022-11-22 16:00:05 +0300

    Added eat_calendar logic

---
+++ src/main/java/com/example/trainhome/userservice/repositories/EatCalendarRepository.java
@@ -0,0 +1,36 @@
+package com.example.trainhome.userservice.repositories;
+
+import com.example.trainhome.userservice.entities.EatCalendar;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface EatCalendarRepository extends JpaRepository<EatCalendar, Long> {
+
+    @Query(value = "select * from eat_calendar where id =: id", nativeQuery = true)
+    EatCalendar getById(@Param("id") Long id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "delete from eat_calendar where id =: id", nativeQuery = true)
+    void deleteById(@Param("id") Long id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "update eat_calendar set date =: date, info =: info where id =: id", nativeQuery = true)
+    int updateRecommendation(@Param("id") Long id, @Param("date") Date date, @Param("info") String info);
+
+    @Query(value = "select * from eat_calendar where coach_id =: coachId and person_id =: personId and date =: date", nativeQuery = true)
+    EatCalendar getByPersonIdAndCoachIdAndDate(@Param("coachId") Long coachId, @Param("personId") Long personId,
+                                               @Param("date") Date date);
+
+    @Query(value = "select * from eat_calendar where person_id =: personId and date =: date", nativeQuery = true)
+    List<EatCalendar> getAllByPersonIdAndDate(@Param("personId") Long personId, @Param("date") Date date);
+}
--- src/main/java/com/example/trainhome/userservice/repositories/PersonRepository.java
+++ src/main/java/com/example/trainhome/userservice/repositories/PersonRepository.java
@@ -1,6 +1,8 @@
 package com.example.trainhome.userservice.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.example.trainhome.userservice.entities.Person;
 
@@ -10,4 +12,7 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
     Person findByEmail(String email);
 
     Person deletePerson(Person person);
+
+    @Query(value = "select * from person where id =: id", nativeQuery = true)
+    Person getById(@Param("id") Long id);
 }
